0eabd1cd2385c03a7b473c040963f383172139f0
Maira Wangui <140074824+MairaQuillQuest@users.noreply.github.com>
2023-12-15 15:36:11 +0300

    Update get_opcodes.c

--- get_opcodes.c
+++ get_opcodes.c
@@ -9,7 +9,7 @@
  */
 void (*get_opcode_func(char *opc))(stacknode_t **stacknode, unsigned int line_number)
 {
-	opfunc_t instruct[] = {
+	opFunc_t instruct[] = {
 		{"push", monty_push},
 		{"pall", monty_pall},
 		{"pint", monty_pint},
