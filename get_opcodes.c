2f7642992bed2464c1f342dcba992d44e1dd16ab
Maira Wangui <140074824+MairaQuillQuest@users.noreply.github.com>
2023-12-15 13:53:23 +0300

    Create get_opcodes.c

---
+++ get_opcodes.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * get_opcode_func - selects the correct opcode to perform
+ *
+ * @opc: represents opcode passed
+ *
+ * Return: returns pointer to the function that executes the opcode
+ */
+void (*get_opcode_func(char *opc))(stacknode_t **stacknode, unsigned int line_number)
+{
+	instruction_t instruct[] = {
+		{"push", monty_push},
+		{"pall", monty_pall},
+		{"pint", monty_pint},
+		{"pop", monty_pop},
+		{"swap", monty_swap},
+		{"queue", monty_queue},
+		{"stack", monty_stack},
+		{"add", monty_add},
+		{"nop", monty_nop},
+		{"sub", monty_sub},
+		{"mul", monty_mul},
+		{"div", monty_div},
+		{"mod", monty_mod},
+		{"pchar", monty_pchar},
+		{"pstr", monty_pstr},
+		{"rotl", monty_rotl},
+		{"rotr", monty_rotr},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instruct[i].opcode; i++)
+	{
+		if (custom_strcmp(instruct[i].opcode, opc) == 0)
+			break;
+	}
+
+	return (instruct[i].f);
+}
