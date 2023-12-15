b74e474d4239fcfe1e0b6cf72ddf54b563368dbe
Maira Wangui <140074824+MairaQuillQuest@users.noreply.github.com>
2023-12-15 18:48:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,20 +12,20 @@ int main(int argc, char *argv[])
     void (*f)(stacknode_t **stacknode, unsigned int line_number);
     FILE *file_desc;
     size_t size = 256;
-    char *line = NULL;
+    char line[256];
 
     file_desc = check_input(argc, argv);
     start_g_data(file_desc);
 
-    while (getline(&line, &size, file_desc) != -1)
+    while (fgets(line, sizeof(line), file_desc) != NULL)
     {
         char *lines[2] = {NULL, NULL};
         lines[0] = _custom_strtok(line, " \t\n");
-        
+
         if (lines[0] && lines[0][0] != '#')
         {
             f = get_opcode_func(lines[0]);
-            
+
             if (!f)
             {
                 fprintf(stderr, "L%u: ", g_data.cur_line);
@@ -33,15 +33,14 @@ int main(int argc, char *argv[])
                 free_g_data();
                 exit(EXIT_FAILURE);
             }
-            
+
             g_data.sec_param = _custom_strtok(NULL, " \t\n");
             f(&g_data.dll_head, g_data.cur_line);
         }
-        
+
         g_data.cur_line++;
     }
 
-    free(line);
     free_g_data();
 
     return (0);
